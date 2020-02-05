a023ceec515ee781523619253aaf0aaf43111038
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-02-05 22:10:41 +0100

    Add jQuery isotope

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
+    const $container = $('.projects');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.filterBox a').click(function(){
+        $('.filterBox .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        const selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
 });
