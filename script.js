7ddb82c87d1f304064d4752d93b717dc340661e3
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-05-01 14:59:25 +0200

    Delete isotope

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-$(window).load(function() {
-    const $container = $('.projects');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 1000,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
-    $('.filterBox a').click(function(){
-        $('.filterBox .current').removeClass('current');
-        $(this).addClass('current');
- 
-        const selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    //PORTFOLIO
 
+    //CONTACT
     const emailButton = document.querySelector('.emailButton');
     const email = document.querySelector('.email');
 
